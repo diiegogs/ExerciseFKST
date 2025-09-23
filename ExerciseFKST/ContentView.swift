0d7407b3a3b878300962d46bb39e893d55c1ce96
JDiego-Garcia <diiegogarcs26@gmail.com>
2025-09-23 15:01:54 -0600

    [FTR]: start project configuration

---
+++ ExerciseFKST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExerciseFKST
+//
+//  Created by JJuan Diego Garcia Serrano on 23/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExerciseFKST/ExerciseFKSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseFKSTApp.swift
+//  ExerciseFKST
+//
+//  Created by JJuan Diego Garcia Serrano on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseFKSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
